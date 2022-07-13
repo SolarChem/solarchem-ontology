04b7ea440ab8a5e55083afa50e7db3a125de105a
Oier Beaskoetxea <obeaskoetxea@gmail.com>
2022-07-13 12:51:04 +0200

    Generado autoejecutable de extracción y mapeo

---
+++ data_mappeator/data_mapping.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+START=$(date +%s)
+
+echo "Starting with data extraction..."
+python3 db_data__extraction/main.py
+
+END=$(date +%s)
+DIFF=$(( $END - $START ))
+
+echo "Data extraction done. Time needed: $DIFF seconds"
+
+START=$(date +%s)
+
+echo "Starting with the mapping process..."
+java -jar r2rml.jar ./config.properties
+
+END=$(date +%s)
+DIFF=$(( $END - $START ))
+
+echo "Mapping done. Time needed: $DIFF seconds"
