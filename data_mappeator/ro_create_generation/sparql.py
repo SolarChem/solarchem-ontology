fd9ab6e22800b33ba1e23b5eae7e59816e1602f2
oibeal <obeaskoetxea@gmail.com>
2022-07-25 10:22:23 +0200

    Factorizada función con construct de sparql

--- data_mappeator/ro_create_generation/sparql.py
+++ data_mappeator/ro_create_generation/sparql.py
@@ -12,40 +12,75 @@ def get_all_article_iris(graph):
 
 def create_article_rdf(graph, iri):
     doi = iri.split("/")[-1]
+    qconstruct = graph.query(get_article_rdf_query(iri))
+    filename = parser.get('files', 'output_name')+"_"+doi
+    filedir = parser.get('files', 'output_directory')+"/"+filename+".rdf"
+    qconstruct.serialize(filedir)
 
-    query = """
+def get_article_rdf_query(iri) -> str:
+    return """
         PREFIX dcterms: <http://purl.org/dc/terms/>
         PREFIX phcat: <http://base.namespace.com/>
 
         CONSTRUCT { 
-            <%s> ?article_property ?article_value . 
-            ?journal ?journal_property ?journal_value .
-            ?creator ?creator_property ?creator_value .
-            ?country ?country_property ?country_value .
+            <%s> ?article_prop ?article_val . 
+            ?journal ?journal_prop ?journal_val .
+            ?creator ?creator_prop ?creator_val .
+            ?country ?country_prop ?country_val .
+            ?mat_trans ?mat_trans_prop ?mat_trans_val .
+            ?yield ?yield_prop ?yield_val .
+            ?eg ?eg_prop ?eg_val .
+            ?bet ?bet_prop ?bet_val .
+            ?condition ?condition_prop ?condition_val .
+            ?input ?input_prop ?input_val .
+            ?output ?output_prop ?output_val .
+            ?chem_in ?chem_in_prop ?chem_in_val .
+            ?chem_out ?chem_out_prop ?chem_out_val .
+            ?output_q ?output_q_prop ?output_q_val .
         }
         WHERE { 
-            <%s> ?article_property ?article_value ;
+            <%s> ?article_prop ?article_val ;
                  dcterms:isPartOf  ?journal ;
                  dcterms:creator   ?creator ;
                  phcat:hasMaterialTransformationProcess ?mat_trans .
 
-                ?journal ?journal_property ?journal_value .
+            ?journal ?journal_prop ?journal_val .
 
-                ?creator ?creator_property ?creator_value .
+            ?creator ?creator_prop ?creator_val .
 
-                OPTIONAL { ?creator phcat:affiliationCountry ?country . 
-                           ?country ?country_property ?country_value .
-                }
+            ?mat_trans ?mat_trans_prop ?mat_trans_val ;
+                       phcat:hasInput ?input ;
+                       phcat:hasOutput ?output .
+            
+            ?input ?input_prop ?input_val ;
+                   phcat:hasChemical ?chem_in .
+
+            ?output ?output_prop ?output_val ;
+                    phcat:hasOutputQuantity ?output_q ;
+                    phcat:hasChemical ?chem_out .
+
+            ?chem_in ?chem_in_prop ?chem_in_val .
+            ?chem_out ?chem_out_prop ?chem_out_val .
+            ?output_q ?output_q_prop ?output_q_val .
+
+            OPTIONAL { 
+                ?creator phcat:affiliationCountry ?country . 
+                ?country ?country_prop ?country_val .
+
+                ?mat_trans phcat:hasYield ?yield ;
+                           phcat:hasReactionEg ?eg ;
+                           phcat:hasBET ?bet ;
+                           phcat:hasCondition ?condition .
+
+                ?yield ?yield_prop ?yield_val .
+                ?eg ?eg_prop ?eg_val .
+                ?bet ?bet_prop ?bet_val .
+                ?condition ?condition_prop ?condition_val .
+            }
         }
     """ % (iri, iri)
-
-    qconstruct = graph.query(query)
-    filename = parser.get('files', 'output_name')+"_"+doi
-    filedir = parser.get('files', 'output_directory')+"/"+filename+".rdf"
-    qconstruct.serialize(filedir)
 
 def load_graph():
     g = rdflib.Graph()
     g.parse(parser.get('files', 'input'), format="turtle")
     return g
-
