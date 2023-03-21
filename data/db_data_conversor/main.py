be67aa6199a25b81d9e6b1cac2cdd57c915a8fda
Ana Iglesias <ana.iglesias.molina@gmail.com>
2023-03-21 18:56:54 +0100

    restructuring and new diagram version

---
+++ data/db_data_conversor/main.py
@@ -0,0 +1,25 @@
+from data_inicializator import *
+from utils import *
+
+#Preparamos la información
+articles = prepare_articles()
+journals = prepare_journals(articles)
+authors = prepare_authors(articles)
+material_tranformations = prepare_material_transformations(articles)
+inputs = prepare_inputs(material_tranformations)
+conditions = prepare_conditions(material_tranformations)
+outputs = prepare_outputs(material_tranformations)
+chemicals = prepare_chemicals([*inputs, *outputs])
+units = prepare_units([*conditions, *outputs])
+
+
+#Generamos los csv
+createCsv(articles, 'paper_references.csv')
+createCsv(journals, 'journals.csv')
+createCsv(authors, 'authors.csv')
+createCsv(material_tranformations, 'material_transformations.csv')
+createCsv(inputs, 'inputs.csv')
+createCsv(conditions, 'conditions.csv')
+createCsv(outputs, 'outputs.csv')
+createCsv(chemicals, 'chemicals.csv')
+createCsv(units, 'units.csv')
--- new-case-story.md
+++
@@ -1,3 +0,0 @@
-# Case Story
-
-*Describe the story according to the structure: As (actor) I want (something) to get (benefit)".*
