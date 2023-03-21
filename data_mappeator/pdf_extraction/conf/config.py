c5317b992a1f57eb69fe9b7d0102a43dfbde3e42
Ana Iglesias <ana.iglesias.molina@gmail.com>
2023-03-21 18:58:31 +0100

    restructuring

--- data_mappeator/pdf_extraction/conf/config.py
+++
@@ -1,7 +0,0 @@
-import configparser
-import os
-
-parser = configparser.ConfigParser()
-
-conf_file = os.path.join(os.path.dirname(__file__), 'settings.conf')
-parser.read(conf_file)
--- data_mappeator/sparql_queries/const.py
+++
@@ -1,6 +0,0 @@
-CATALYST = "<http://base.namespace.com/Catalyst>"
-CO_CATALYST = "<http://base.namespace.com/Catalyst>"
-DOPANT = "http://base.namespace.com/Dopant"
-DYE = "http://base.namespace.com/Dye"
-REDUCTANT = "http://base.namespace.com/Reductant"
-SUPPORT = "http://base.namespace.com/Support"
